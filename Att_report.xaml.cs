cd2b7704e47a3aee2d41f9e2d0f61fd38dfbc843
TRINETRA DEVKATTE <tridev09.td@outlook.com>
2018-11-22 21:59:10 +0530

    Made changes to Report Generation

--- Att_report.xaml.cs
+++ Att_report.xaml.cs
@@ -26,6 +26,7 @@ namespace WpfApp2
     {
         AttendanceReport report;
         ObservableCollection<Test> col;
+        String Date,Sub;
 
         internal Att_report(AttendanceReport r)
         {
@@ -33,9 +34,11 @@ namespace WpfApp2
             report = r;
             Subject s = (Subject)(r.subject);
             LSubject.Content = s.Name;
+            Sub = s.Name;
             LCourseCode.Content = s.Code;
             LFacultyName.Content = s.FacultyName;
             LDate.Content = r.Date.ToLongDateString();
+            Date= r.Date.ToLongDateString();
             col = new ObservableCollection<Test>();
             foreach (var k in report.ConvertToList())
             {
@@ -69,57 +72,22 @@ namespace WpfApp2
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-           /* var res = col.ToArray();
-            for (int i = 0; i < res.Length; i++){report.Present[i] = res[i].Test3;}
-            var text = report.ToString(); Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-            dlg.FileName = ((Subject)report.subject).Code; // Default file name
-            dlg.DefaultExt = ".txt"; // Default file extension
-            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
-
-            // Show save file dialog box
-            Nullable<bool> result = dlg.ShowDialog();
-            // Process save file dialog box results
-            if (result == true)
-            {
-                // Save document
-                string path = dlg.FileName;
-                System.IO.File.WriteAllText(path,text);
-            }*/
             Export_Excel(report.ConvertToList());
             Report_button.Content = "Report Generated";
+            Report_button.IsEnabled = false;
         }
 
-        /* public void Export_Excel(IEnumerable<Data_Diaplay> Info)
-         {
-             Workbook workbook = new Workbook();
-             Worksheet sheet = workbook.Worksheets[0];
-             sheet.Name = "Attendance";
-             sheet.Range["A1"].Text = "Roll NO";
-             sheet.Range["B1"].Text = "Name";
-             sheet.Range["C1"].Text = "Present";
-             int i = 2;
-             foreach(var data in Info)
-             {
-                 i++;
-                 sheet.Range[("A" + i)].Text = data.roll_no;
-                 sheet.Range[("B" + i)].Text = data.name;
-                 sheet.Range[("C" + i)].Text = data.present;
-             }
-             DateTime time = DateTime.Now;
-
-             workbook.SaveToFile("report.xls");
-             System.Diagnostics.Process.Start("report.xls");
-         }*/
+        
 
         public void Export_Excel(IEnumerable<Data_Diaplay> Info)
         {
             SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
             ExcelFile myExcelFile = new ExcelFile();
-            ExcelWorksheet excWsheet = myExcelFile.Worksheets.Add("Hajan's worksheet");
+            ExcelWorksheet excWsheet = myExcelFile.Worksheets.Add("Attendance worksheet");
 
             excWsheet.Columns[0].Width = 10 * 256;
             excWsheet.Columns[1].Width = 30 * 256;
-            excWsheet.Columns[2].Width = 10 * 256;
+            excWsheet.Columns[2].Width = 15 * 256;
 
             excWsheet.Rows[2].Cells[0].Value = "Roll No";
             excWsheet.Rows[2].Cells[1].Value = "Name";
@@ -134,7 +102,7 @@ namespace WpfApp2
                 count++;
 
             }
-            myExcelFile.Save(@"C:\etc\Myfile");
+            myExcelFile.Save(@"C:\etc\"+Sub+" "+Date+" .xls");
         }
     }
 
